bc2f12bfd237f0dc75afdf2fdc8cf76487d6e27a
andrescol24 <andresmil24@hotmail.com>
2021-10-31 15:58:34 -0500

    proyect initilize

---
+++ src/main/java/com/thales/salary/api/salaryapi/SalaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.thales.salary.api.salaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thales/salary/api/salaryapi/SalaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thales.salary.api.salaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
